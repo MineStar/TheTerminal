76d663fa3495dceaba07cdf7448ce79a3ae5676e
GeMoschen <x-chris-x@arcor.de>
2012-03-31 20:06:26 +0200

    - we need a login!

--- src/main/java/de/minestar/theterminal/http/TemplateFinder.java
+++ src/main/java/de/minestar/theterminal/http/TemplateFinder.java
@@ -5,6 +5,7 @@ import com.bukkit.gemo.BukkitHTTP.Page;
 
 import de.minestar.theterminal.http.pages.tplDoChat;
 import de.minestar.theterminal.http.pages.tplGetChat;
+import de.minestar.theterminal.http.pages.tplLogin;
 
 public class TemplateFinder {
     public static Page findTemplate(Page page, HTTPEvent event) {
@@ -12,6 +13,8 @@ public class TemplateFinder {
             tplGetChat.execTemplate(page, event);
         } else if (event.fileName.equalsIgnoreCase("doChat.html")) {
             tplDoChat.execTemplate(page, event);
+        } else if (event.fileName.equalsIgnoreCase("login.html")) {
+            tplLogin.execTemplate(page, event);
         }
         return page;
     }
--- src/main/java/de/minestar/theterminal/http/TerminalHTTP.java
+++ src/main/java/de/minestar/theterminal/http/TerminalHTTP.java
@@ -1,5 +1,7 @@
 package de.minestar.theterminal.http;
 
+import java.util.HashMap;
+
 import com.bukkit.gemo.BukkitHTTP.HTTPEvent;
 import com.bukkit.gemo.BukkitHTTP.HTTPPlugin;
 import com.bukkit.gemo.BukkitHTTP.Page;
@@ -32,5 +34,30 @@ public class TerminalHTTP extends HTTPPlugin {
     @Override
     public void handlePostRequest(Page page, HTTPEvent event) {
         TemplateFinder.findTemplate(page, event);
+    }
+
+    // /////////////////////
+    //
+    // LOGIN SUCCESSFUL
+    //
+    // /////////////////////
+    @Override
+    public String loginSuccessful(HTTPEvent event) {
+        try {
+            HashMap<String, String> param = event.postParameter;
+            if (param == null)
+                return null;
+
+            if (param.size() < 2)
+                return null;
+
+            if (param.get("password").equalsIgnoreCase("test") && param.get("username").equalsIgnoreCase("admin")) {
+                return "Username=" + "admin";
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
